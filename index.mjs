1622c2380bea26a2b664d23247b6399fb5d113b3
scapula07 <bartholomewonogwu@yahoo.com>
2022-08-03 13:55:31 -0400

    first commit,bug detected in mjs file

---
+++ index.mjs
@@ -0,0 +1,111 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
+
+const startingBalance = stdlib.parseCurrency(100);
+
+const accCreator=await stdlib.newTestAccount(startingBalance)
+console.log("creating nft")
+
+const audioNFT=await stdlib.launchToken(accCreator,"AudioMix","AMT",{supply:1})
+const nftId =audioNFT.id 
+const minBid =stdlib.parseCurrency(2);
+const lenInBlock =10
+const  params={nftId,minBid,lenInBlock}
+
+let done=false
+const bidders=[]
+const startBidders=async()=>{
+  let bid=minBid
+
+  const runBidder=async(who)=>{
+    const inc =stdlib.parseCurrency(Math.random()*10)
+    bid=bid.add(inc)
+
+    const acc=await stdlib.newTestAccount(startingBalance)
+    acc.setDebugLabel(who)
+    await acc.tokenAccept(nftId)
+    bidders.push([who,acc])
+    const ctc=acc.contract(backend,ctcCreator.getInfo())
+  const getBal =async()=stdlib.formatCurrency(await stdlib.balanceOf(acc))
+
+  console.log(`${who} decides to bid ${stdlib.formatCurrency(bid)}`)
+  console.log(`${who} decides to before is ${await getBal()}`)
+  try{
+    const [lastBidder,lastBid] = await ctc.apis.Bidder.bid(bid)
+     console.log(`${who} out bid ${lastBidder} who bid ${stdlib.formatCurrency(lastBid)}`) 
+  }catch(e){
+    console.log("Auction is over")
+  }
+  console.log(`${who} decides to after is ${await getBal()}`)
+  }
+
+  await runBidder("Alice")
+  await runBidder("Bob")
+  await runBidder("Claire")
+
+  while(!done){
+    await stdlib.wait(1)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const ctcCreator=accCreator.contract(backend)
+await ctcCreator.p.Creator({
+
+  getSale:()=>{
+    console.log("set sale params")
+
+    return params
+  },
+
+  auctionReady:()=>{
+    startBidders();
+  },
+
+  seeBid:(who,amt)=>{
+    console.log(`Creator saw that ${stdlib.formatAddress(who) }bid ${stdlib.formatCurrency(amt)}`)
+  },
+  showOutcome:(winner,amt)=>{
+    console.log(`Creator saw that ${stdlib.formatAddress(winner) }bid ${stdlib.formatCurrency(amt)}`)
+
+  }
+})
+
+for (const [who,acc] of bidders){
+  const [amt,amtNFT]=await stdlib.balanceOf(acc,[null,nftId])
+  console.log(`${who} has ${stdlib.formatCurrency(amt)} ${stdlib.standardUnit} and ${amtNFT}`)
+}
+
+ done =true
---
+++ index.rsh
@@ -0,0 +1,64 @@
+'reach 0.1';
+
+export const main = Reach.App(() => {
+  const Creator = Participant('Creator', {
+     getSale:Fun([],Object({
+      nftId:Token,
+      minBid:UInt,
+      lenInBlock:UInt
+
+     })),
+     auctionReady:Fun([],Null),
+     showBid:Fun([Address,UInt],Null),
+     showOutcome:Fun([Address,UInt],Null)
+  });
+  const Bidder= API('Bidder', {
+    // Specify Bob's interact interface here
+    bid:Fun([UInt],Tuple(Address,UInt))
+  });
+  init();
+
+  Creator.only(()=>{
+    const {nftId,minBid,lenInBlock} =declassify(interact.getSale())
+
+  })
+
+  Creator.publish(nftId,minBid,lenInBlock)
+  const amt =1
+  commit()
+  Creator.pay([[amt,nftId]])
+  Creator.interact.auctionReady()
+  assert(balance(nftId)==amt,"Balance of Nft is wrong")
+
+  const end=lastConsensusTime() +lenInBlock
+
+  const [highestBidder,lastPrice,isFirstBid]
+    =parallelReduce([ Creator,minBid,true ])
+    .invariant(balance(nftId) == amt)
+    .invariant(balance() == (isFirstBid? 0:lastPrice))
+    .while(lastConsensusTime() <= end)
+    .api_(Bidder.bid, (bid) => {
+       check(bid >lastPrice,"bid is too low")
+      return[bid,(notify) =>{
+        notify([highestBidder, lastPrice])
+        if(!isFirstBid){
+          transfer(lastPrice).to(highestBidder)
+        }
+        const who=this
+        Creator.interact.showBid(who,bid)
+        return[who,bid,false]
+      }]
+    })
+    .timeout(absoluteTime(end), () => {
+        Creator.publish();
+      
+        return [highestBidder,lastPrice,isFirstBid]
+    });
+    transfer(amt,nftId).to(highestBidder)
+    if(!isFirstBid){
+      transfer(lastPrice).to(Creator)
+    }
+    Creator.interact.showOutcome(highestBidder,lastPrice)
+    commit()
+    exit()
+});
